9e9cb833698642c024eb0ff8adc14bb3e1d8bca6
ivangarmir18 <ivangarmir18@gmail.com>
2025-12-28 11:45:52 +0100

    v1.2

---
+++ api/lastest.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const channelId = "UCQ-jmzE0enmyz1ajeXte2lw";
+  const rssUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
+  const response = await fetch(rssUrl);
+  const text = await response.text();
+
+  // Buscar todos los <link> y descartar shorts
+  const links = [...text.matchAll(/<link rel="alternate" href="([^"]+)"/g)]
+                    .map(m => m[1])
+                    .filter(link => !link.includes("/shorts/"));
+
+  if (links.length > 0) {
+    res.writeHead(302, { Location: links[0] });
+    res.end();
+  } else {
+    res.status(404).send("No se encontró un vídeo largo");
+  }
+}
